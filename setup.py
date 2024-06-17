755e34ca92c4590ea3169b53d567aae6ec4051b5
Gypsophila <57577005+Ketherine0@users.noreply.github.com>
2024-06-17 11:28:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2",
-               "rpy2>=3.5.5","torch>=1.9.0","SimpleITK>=2.2.0","nibabel>=5.0.0",
-               "scipy>=1.7.1","tqdm>=4.64.1","scikit-learn>=1.2.1",]
+with open('requirements.txt') as f:
+    install_requires = [
+        line.strip() for line in f.readlines() if line.strip()
+    ]
 
 setup(
-    name="pkg_bnnstgp1",
-    version="0.1.5",
+    name="bnnstgp",
+    version="0.1",
     author="Ella he",
     author_email="ellahe@umich.edu",
     description="BNNSTGP",
